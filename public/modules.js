30b932c1ea59e65a664aae2875175ebadb489ba7
JshStadler <37085670+JshStadler@users.noreply.github.com>
2026-07-10 19:21:41 +0200

    Refactor tracker module data into separate file

---
+++ public/modules.js
@@ -0,0 +1,75 @@
+export const YEAR = 2026;
+export const DUE_HOUR = 20;
+export const DUE_SOON_DAYS = 7;
+export const OPEN_HORIZON_DAYS = 30;
+
+export const MODULES = [
+  { level: 1, code: 'COS1501', title: 'Theoretical Computer Science I', color: 'var(--c-cos1501)', moodleId: 86818, assessments: [
+    { n: 1, done: true, due: '30 Apr' },
+    { n: 2, done: true, open: '29 Apr', due: '8 Jun' },
+    { n: 3, open: '4 May', due: '31 Jul' },
+    { n: 4, open: '3 Aug', due: '11 Sep' },
+  ]},
+  { level: 1, code: 'COS1511', title: 'Introduction to Programming I', color: 'var(--c-cos1511)', moodleId: 86821, assessments: [
+    { n: 1, done: true, open: '1 May', due: '15 May' },
+    { n: 2, done: true, due: '24 Jun' },
+    { n: 3, done: true, due: '14 Aug' },
+    { n: 4, open: '1 Jul', due: '9 Sep' },
+  ]},
+  { level: 1, code: 'COS1512', title: 'Introduction to Programming II', color: 'var(--c-cos1512)', moodleId: 86823, assessments: [
+    { n: 1, done: true },
+    { n: 2, open: '15 Jun', due: '28 Jul' },
+    { n: 3, open: '8 Jul', due: '18 Aug' },
+    { n: 4, open: '24 Jul', due: '10 Sep' },
+  ]},
+  { level: 1, code: 'COS1521', title: 'Computer Systems: Fundamental Concepts', color: 'var(--c-cos1521)', moodleId: 86824, assessments: [
+    { n: 1, done: true },
+    { n: 2, done: true },
+    { n: 3, done: true, open: '1 Apr', due: '22 Jul' },
+    { n: 4, due: '13 Aug' },
+    { n: 5, due: '2 Sep' },
+  ]},
+  { level: 1, code: 'INF1505', title: 'Introduction to Business Information Systems', color: 'var(--c-inf1505)', moodleId: 87486, assessments: [
+    { n: 1, done: true, open: '24 Apr', due: '5 May' },
+    { n: 2, done: true, open: '1 Jun', due: '17 Jun' },
+    { n: 3, open: '29 Jul', due: '11 Aug' },
+    { n: 4, open: '7 Sep', due: '14 Sep' },
+  ]},
+  { level: 1, code: 'MAT1503', title: 'Linear Algebra I', color: 'var(--c-mat1503)', moodleId: 88600, assessments: [
+    { n: 1, done: true, open: '1 Apr', due: '29 May' },
+    { n: 2, done: true, open: '30 May', due: '30 Jun' },
+    { n: 3, open: '1 Apr', due: '31 Jul' },
+    { n: 4, open: '1 Jul', due: '31 Aug' },
+    { n: 5, due: '11 Sep' },
+  ]},
+  { level: 1, code: 'INF1511', title: 'Visual Programming I', color: 'var(--c-inf1511)', moodleId: 87487, assessments: [
+    { n: 1, done: true },
+    { n: 2, done: true },
+    { n: 3, done: true, open: '27 Apr', due: '14 Sep' },
+    { n: 4, done: true, open: '18 May', due: '14 Sep' },
+    { n: 5, done: true, open: '8 Jun', due: '14 Sep' },
+    { n: 6, done: true, open: '6 Jul', due: '14 Sep' },
+    { n: 7, open: '27 Jul', due: '14 Sep' },
+    { n: 8, open: '17 Aug', due: '14 Sep' },
+  ]},
+  { level: 1, code: 'MAT1512', title: 'Calculus A', color: 'var(--c-mat1512)', moodleId: 88603, assessments: [
+    { n: 2, done: true, open: '1 Apr', due: '26 May' },
+    { n: 4, done: true, due: '22 Jun' },
+    { n: 5, due: '27 Jul' },
+    { n: 6, due: '17 Aug' },
+  ]},
+  { level: 2, code: 'COS2626', title: 'Computer Networks I', color: 'var(--c-cos2626)', moodleId: 86834, assessments: [
+    { n: 1, done: true },
+    { n: 2, done: true, open: '13 May', due: '10 Jun' },
+    { n: 3, done: true, due: '8 Jul' },
+    { n: 4, due: '12 Aug' },
+    { n: 5, due: '26 Aug' },
+  ]},
+  { level: 2, code: 'STA1610', title: 'Introduction to Statistics', color: 'var(--c-sta1610)', moodleId: 87569, assessments: [
+    { n: 1, done: true, open: '4 May', due: '11 May' },
+    { n: 2, done: true, open: '2 Jun', due: '9 Jun' },
+    { n: 3, done: true, open: '30 Jun', due: '6 Jul' },
+    { n: 4, open: '28 Jul', due: '4 Aug' },
+    { n: 5, open: '11 Aug', due: '31 Aug' },
+  ]},
+];
