fcca60dde4c1528c67a860a18421ce34c7a2a79c
JshStadler <37085670+JshStadler@users.noreply.github.com>
2026-08-19 16:07:01 +0200

    Update modules.js

--- public/modules.js
+++ public/modules.js
@@ -50,7 +50,7 @@ export const MODULES = [
     { n: 5, done: true, open: '8 Jun', due: '14 Sep' },
     { n: 6, done: true, open: '6 Jul', due: '14 Sep' },
     { n: 7, done: true, open: '27 Jul', due: '14 Sep' },
-    { n: 8, open: '17 Aug', due: '14 Sep' },
+    { n: 8, done: true, open: '17 Aug', due: '14 Sep' },
   ]},
   { level: 1, code: 'MAT1512', title: 'Calculus A', color: 'var(--c-mat1512)', moodleId: 88603, assessments: [
     { n: 1, done: true, open: '1 Apr', due: '26 May' },
