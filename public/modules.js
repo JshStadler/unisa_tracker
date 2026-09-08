1d30be35a84d12d5437e146c320ed88daf7d87f7
JshStadler <37085670+JshStadler@users.noreply.github.com>
2026-09-08 23:17:56 +0200

    Update modules.js

--- public/modules.js
+++ public/modules.js
@@ -19,7 +19,7 @@ export const MODULES = [
   { level: 1, code: 'COS1512', title: 'Introduction to Programming II', color: 'var(--c-cos1512)', moodleId: 86823, assessments: [
     { n: 1, done: true },
     { n: 2, done: true, open: '15 Jun', due: '28 Jul' },
-    { n: 3, open: '8 Jul', due: '18 Aug' },
+    { n: 3, done: true, open: '8 Jul', due: '18 Aug' },
     { n: 4, done: true, open: '24 Jul', due: '10 Sep' },
   ]},
   { level: 1, code: 'COS1521', title: 'Computer Systems: Fundamental Concepts', color: 'var(--c-cos1521)', moodleId: 86824, assessments: [
